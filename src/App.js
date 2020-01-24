6eed7de4acdd53606c9ab70e8d64bd09a603249b
Goran.Mitkovski <Goran.Mitkovski@TERMALIFT.LOKAL>
2020-01-24 12:27:36 +0100

    Map function

--- src/App.js
+++ src/App.js
@@ -19,25 +19,44 @@ const list = [
   objectID: 1,
   },
   ];
-
+  
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list,
+      };
+      this.onDismiss = this.onDismiss.bind(this);
+    }
+    
+    onDismiss(id) {
+      const isNotId = item => item.objectID !== id;
+      const updatedList = this.state.list.filter(isNotId);
+      this.setState({ list: updatedList });
+      }
   render(){
     const helloWorld = 'Welcome Goran to the Road to learn React';
     return (
       <div className="App">
-      {list.map(function(item) {
-      return (
-      <div>
+      {this.state.list.map(item =>
+      <div key={item.objectID}>
       <span>
       <a href={item.url}>{item.title}</a>
       </span>
       <span>{item.author}</span>
       <span>{item.num_comments}</span>
       <span>{item.points}</span>
+      <span>
+          <button
+            onClick={() => this.onDismiss(item.objectID)}
+            type="button"
+            >
+            Dismiss
+          </button>
+      </span>
       </div>
-      );
-      })}
+      )}
       </div>
   );
 }}
